25f04ccaee835e4e17a6e47d7d3fd1e59417ffea
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-05-30 23:05:02 -0300

    STYLES: adding button styles

--- front-end/src/styles/global.ts
+++ front-end/src/styles/global.ts
@@ -1,28 +1,37 @@
 import { globalCss } from ".";
 
 export const globalStyles = globalCss({
-    "*": {
-        margin: 0,
-        padding: 0,
-        boxSizing: "border-box",
-    },
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
 
-    body: {
-        backgroundColor: "$background",    
-    },
+  body: {
+    backgroundColor: "$background",
+  },
 
-    'body, input, button, textarea': {
-        fontFamily: "'Roboto', sans-serif",
-        fontSize: "1rem",
-        fontWeight: 400,
-    },
+  "body, input, button, textarea": {
+    fontFamily: "'Roboto', sans-serif",
+    fontSize: "1rem",
+    fontWeight: 400,
+  },
 
-    a: {
-        color: 'inherit',
-        textDecoration: 'none',
-    },
-    
-    li: {
-        listStyle: 'none',
-    }
-})
+  a: {
+    color: "inherit",
+    textDecoration: "none",
+  },
+
+  li: {
+    listStyle: "none",
+  },
+
+  button: {
+    backgroundColor: "$primary",
+    color: "#fff",
+    padding: "12px 16px",
+    border: "none",
+    borderRadius: "10px",
+    cursor: "pointer",
+  },
+});
