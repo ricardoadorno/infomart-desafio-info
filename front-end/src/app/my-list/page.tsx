1e53a9ed965559f1b901886e91686ab2dfccb3bd
Jean Victor <jeamsillva3@gmail.com>
2023-06-07 08:58:52 -0300

    my-list style

--- front-end/src/app/my-list/page.tsx
+++ front-end/src/app/my-list/page.tsx
@@ -1,5 +1,24 @@
+import Item from "./itens";
+import { ListContainer, ListContent } from "./styles";
+
 export default function MyList(){
     return (
-        <h1>Minha lista</h1>
+        <ListContainer>
+            <h1 className="h2">Sua lista</h1>
+            <ListContent>
+            {/*     <section className="empty">
+                    <h1 className="h2">Sua lista está vazia!</h1>
+                    <p className="emptyDescription h4">
+                        Para inserir produtos basta clicar no botão “+ Lista” debaixo dos itens!
+                    </p>
+                    <a className="homeButton" href="/">Home</a>
+                </section> */}
+
+                <Item />
+     
+            </ListContent>
+         
+        </ListContainer>
+        
     )
 }
